1deac17e28bea235a90c46e1193419e435625a1e
dpamtst <deepam.makwana@tsttechnology.io>
2025-01-08 15:35:50 +0530

    Update ec2_hosting.py

--- ec2_hosting.py
+++ ec2_hosting.py
@@ -1,6 +1,7 @@
 import os
 os.system("sudo apt update")
 os.system("sudo apt -y install nginx certbot python3-certbot-nginx nodejs npm")
+os.system("npm install -g pm2")
 repo_name=input("Enter the name of the repository: ")
 os.system(f"git clone {repo_name}")
 os.system(f"cd {repo_name}")
